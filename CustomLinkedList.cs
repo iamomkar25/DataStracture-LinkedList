03de0bdd19073f3335e67a82f96af3f969aa56ae
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-19 00:30:29 +0530

    adding-ability-to-create-linked-list

--- CustomLinkedList.cs
+++ CustomLinkedList.cs
@@ -56,5 +56,16 @@ namespace linked_list
 
             }
         }
+
+        public void AddAtFirst(int new_data)
+        {
+                                                      // creating a node
+            Node new_node = new Node(new_data);
+                                                     // when we want to add the node at front
+            new_node.next = this.head;
+                                                    //the previous Head node is no=w the second node of linked list
+            this.head = new_node;
+            Console.WriteLine("{0} Node inserted into linkedlist", new_node.data);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace linked_list
        
             static void Main(string[] args)
             {
-                // LinkedList<int> my_line = new LinkedList<int>();
-                // my_line.AddLast(10);
-                // my_line.AddLast(70);
-                //my_line.AddFirst(300);
-                CustomLinkedList linkedList = new CustomLinkedList();
-                linkedList.AddAtLast(30);
-                linkedList.AddAtLast(40);
-                linkedList.AddAtLast(50);
-                linkedList.Display();
-                Console.ReadLine();
-            }
+            // LinkedList<int> my_line = new LinkedList<int>();
+            // my_line.AddLast(10);
+            // my_line.AddLast(70);
+            //my_line.AddFirst(300);
+            CustomLinkedList linkedList = new CustomLinkedList();
+            linkedList.AddAtFirst(30);
+            linkedList.AddAtFirst(40);
+            linkedList.AddAtFirst(60);
+            linkedList.Display();
+            Console.ReadLine();
+        }
 
 
         }
